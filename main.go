780620408745c8419df5e91f3b42cccde7325959
Siddharth pandey <44026219+sid288791@users.noreply.github.com>
2023-04-12 11:13:22 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,4 +3,21 @@ package main
 import(
 	"crypto"
 	"golang.org/x/crypto/ripemd160"
+	"pgpcipher-go/openPGP"
 )
+func init(){
+	crypto.RegisterHash(crypto.RIPEMD160,ripemd160.New)
+}
+
+func main(){
+	pgp_pub_key = ""
+    file_to_enc = ""
+    enc_output_file = ""
+    openPGP.EncryptFile(file_to_enc,enc_output_file,pgp_pub_key)
+
+    pgp_priv_key = ""
+    passphrase = ""
+    enc_file = ""
+    dec_output_file = ""
+    openPGP.DecryptFile(enc_file,pgp_priv_key, passphrase, dec_output_file)
+}
